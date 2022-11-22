4499962b6aca4e652a7ea99863d013fd2e07fdd5
grilo13 <pmsgrilo@hotmail.com>
2022-11-22 23:37:15 +0000

    Started racket model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class Racket(models.Model):
+    class RacketBrand(models.TextChoices):
+        VIBORA = 'Vibora', 'vibora'
+        BULLPADEL = 'Bullpadel', 'bullpadel'
+    name = models.CharField(max_length=120)
+    brand = models.CharField(max_length=30, choices=RacketBrand.choices, default=RacketBrand.VIBORA)
+    model = models.IntegerField()
+    availability = models.IntegerField()
+    price = models.FloatField()
+
+    def __str__(self):
+        return "Racket {} from brand {} costs {}".format(self.name, self.brand, self.price)
